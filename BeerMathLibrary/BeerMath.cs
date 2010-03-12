e4bee00230e04625d6e5358495e634435670dbe5
Matt Cooper <vtbassmatt@gmail.com>
2010-03-12 06:44:27 -0800

    First commit - BeerMath can calculate MCUs :)

---
+++ BeerMathLibrary/BeerMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeerMath
+{
+    public class Malt
+    {
+        public static decimal CalculateMcu(decimal GrainLbs, decimal DegreesLovibond, decimal VolumeGallons)
+        {
+            return (GrainLbs * DegreesLovibond) / VolumeGallons;
+        }
+    }
+}
---
+++ BeerMathSampleApp/formMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using BeerMath;
+
+namespace BeerMath.Sample
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void MCU_Click(object sender, EventArgs e)
+        {
+            decimal lbsGrain = decimal.Parse(textBox1.Text);
+            decimal degLovibond = decimal.Parse(textBox2.Text);
+            decimal totalVolume = decimal.Parse(textBox3.Text);
+
+            decimal MCUs = Malt.CalculateMcu(lbsGrain, degLovibond, totalVolume);
+
+            label1.Text = MCUs.ToString();
+        }
+    }
+}
